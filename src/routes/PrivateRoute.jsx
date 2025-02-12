5cb63cd5b204dbce7a9932c247ed83669893a0c2
Ruhul Amin <ruhulaminbabu93@gmail.com>
2025-02-12 14:37:00 +0600

    Implement a loading spinner and use  it

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const PrivateRoute = ({children}) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
   
     if (loading) {
-      return <div>Loading...</div>;
+      return <LoadingSpinner></LoadingSpinner>;
     }
   
     if (!user) {
